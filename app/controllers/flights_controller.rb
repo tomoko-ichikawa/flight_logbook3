042ec2fabbf7dd575e032ecfa005c452e6925154
tomokoichikawa <takator@yahoo.co.jp>
2019-10-31 15:04:11 +0900

    Add:ログ編集画面

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
+  before_action :set_flight, only: [:show, :edit, :update, :destroy]
+	
 	def index
 		@flights = Flight.all
 	end
@@ -13,12 +15,26 @@ class FlightsController < ApplicationController
 	end
 
 	def show
-		@flight = Flight.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def update
+		if @flight.update(flight_params)
+			redirect_to flights_path,notice: "フライトログを編集しました"
+		else
+			render 'edit'
+		end
 	end
 
 	private
 
 	def flight_params
 		params.require(:flight).permit(:departure_date, :airline, :flight_number, :aircraft_type, :seat_class, :seat_number, :departure_airport, :departure_estimated_time, :actual_departure_time, :arrival_airport, :arrival_estimated_time, :actual_arrival_time, :flight_time, :flight_mile, :fare, :booking_class, :getting_mile, :getting_point, :review, :photo1, :photo2, :photo3, :total_getting_mile, :total_getting_point)
+	end
+
+	def set_flight
+		@flight = Flight.find(params[:id])
 	end
 end
