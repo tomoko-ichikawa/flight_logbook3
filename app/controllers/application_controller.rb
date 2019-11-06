613888cd90baa6350397138d8d262ed65739adc6
tomokoichikawa <takator@yahoo.co.jp>
2019-11-06 14:23:54 +0900

    Add:profile画像

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :icon])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :icon])
 	end
 
 	def after_sign_in_path_for(resource)
