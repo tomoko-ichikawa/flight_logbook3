37c1c19aab944d5c0c3d37d28d3a4d9263f1e214
tomokoichikawa <takator@yahoo.co.jp>
2019-12-13 20:07:30 +0900

    Fix:rspec/user

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -17,19 +17,23 @@ RSpec.feature "Users", type: :feature do
     click_button "Log in"
   end
 
-  scenario "ユーザー一覧のテスト" do
-    login_as_yohei
+  scenario "新規登録が完了すると、ユーザー一覧画面に追加されること" do
+  	visit new_user_registration_path
+    fill_in "user[name]", with: "yohei"
+    fill_in "user[email]", with: "yyy@yyy.com"
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+    click_button "Sign up"
+
     visit users_path
 
   	expect(page).to have_content 'ユーザー一覧'
   end
 
-  scenario "フライト一覧のテスト" do
+  scenario "ログインが完了すると、投稿一覧画面に遷移すること" do
     login_as_yohei
     visit flights_path
 
-  	expect(page).to have_content 'Factoryで作ったデフォルトの航空会社'
+  	expect(page).to have_content '投稿一覧'
   end
-
-
 end
